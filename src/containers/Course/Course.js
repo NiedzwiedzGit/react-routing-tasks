83c795d89a54545f63828ec4aeb4996f34a1504a
Igor Skliarsky <shkliarskiyigor@gmail.com>
2020-04-14 00:23:07 +0200

    react-routing-tasks

---
+++ src/containers/Course/Course.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import '../Courses/Courses.css';
+
+class Course extends Component {
+
+    state = {
+        loadedPostTitle: null
+    }
+    componentDidMount() {
+        //this.setState({ loadedPostTitle: this.props.location.pathname });
+        // console.log(this.props);
+
+        //this.setState({ loadedPostTitle: this.props.match.params.title });
+        // console.log(this.loadedPostTitle);
+    }
+
+    render() {
+        let a = null;
+        if (this.props.match.params.id) {
+            a = <div>
+                <h1>{this.props.match.params.title}</h1>
+                <p>You selected the Course with ID: {this.props.match.params.id}</p>
+            </div>;
+        } else {
+            a = <div>
+                <h1>{this.props.title}</h1>
+                <p>You selected the Course with ID: {this.props.id}</p>
+            </div>;
+        }
+
+        return (
+            <div
+                className="Course"
+                onClick={this.props.clicked}>
+                {a}
+            </div>
+
+        );
+    }
+}
+
+export default withRouter(Course);
---
+++ src/containers/Courses/Courses.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Course from '../Course/Course';
+
+import './Courses.css';
+
+class Courses extends Component {
+    state = {
+        courses: [
+            { id: 1, title: 'Angular - The Complete Guide' },
+            { id: 2, title: 'Vue - The Complete Guide' },
+            { id: 3, title: 'PWA - The Complete Guide' }
+        ],
+        currentUrl: null
+    }
+    postSelectedHandler = (id, title) => {
+        this.props.history.push({ pathname: '/courses/' + id + title });
+    }
+
+
+    render() {
+
+        let course = this.state.courses.map(course => {
+            return (
+                <Course
+                    className="Course"
+                    key={course.id}
+                    id={course.id}
+                    title={course.title}
+                    //match={this.props.match}
+                    clicked={() => this.postSelectedHandler(course.id, course.title)}
+                />
+            );
+        })
+        return (
+            <div>
+                <h1>Amazing Udemy Courses</h1>
+                <section className="Courses">
+                    {
+                        course
+                    }
+                </section>
+                <Route path={this.props.match.url + '/:id' + ':title'} exact component={Course} />
+            </div >
+        );
+    }
+}
+
+export default Courses;
